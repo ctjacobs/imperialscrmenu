a7d557cbb108346c990dea6bb0e1ab72af75e6d5
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-01-20 20:37:37 +0000

    Finished the function that retrieves the HTML from the menu webpage.

--- src/ctjacobs/imperialscrmenu/MainActivity.java
+++ src/ctjacobs/imperialscrmenu/MainActivity.java
@@ -5,6 +5,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.net.*;
 import java.io.*;
+import java.util.regex.*;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -25,22 +26,44 @@ public class MainActivity extends Activity
         DateFormat dateFormat = new SimpleDateFormat("EEEE, dd MMM");
         Date date = new Date();
         textTitle.setText("Menu for " + dateFormat.format(date));
-     
+
+        String text = (String) this.getMenu();
+        System.out.println(text);
+        
         return;
     }
 
     /** Retrieve the menu from the SCR's webpage. */
-    public void getMenu()
+    public String getMenu()
     {
         try
         {
-            URL url = new URL(this.getString(R.string.menu_url));
-            InputStreamReader stream = new InputStreamReader(url.openStream());
-        } 
+            /* Open a connection to the SCR menu's website */
+            URL url = new URL(this.getString(R.string.menu_page_url));
+            URLConnection connection = url.openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible)");
+            connection.setRequestProperty("Accept", "*/*");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            
+            /* Searches for the portion of the HTML that contains the menu title. */
+            String line;
+            Pattern pattern = Pattern.compile(this.getString(R.string.menu_page_url));
+            Matcher matcher;
+            while((line = reader.readLine()) != null)
+            {
+                matcher = pattern.matcher(line);
+                if(matcher.find())
+                {
+                    return line.toString();
+                }
+            }
+        }
         catch(IOException e)
         {
             e.printStackTrace();
         }
-        return;
+        return "";
     }
 }
